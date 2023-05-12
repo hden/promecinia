7a64620a2e7c142240e44c0599ca37b72d3e1b06
Haokang Den <haokang.den@gmail.com>
2023-05-12 10:02:20 +0900

    release v0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.hden/promecinia "0.1.0-SNAPSHOT"
+(defproject com.github.hden/promecinia "0.1.1"
   :description "Use [promesa promises](https://github.com/funcool/promesa) as lacinia ResolverResults."
   :url "https://github.com/hden/promecinia"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
